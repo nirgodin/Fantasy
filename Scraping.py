6f60c35c89ea87cd9ab9ba4aa2201892e8398e87
nirgodin <70273595+nirgodin@users.noreply.github.com>
2021-01-25 15:36:50 +0200

    GW 19b scraping

--- Scraping.py
+++ Scraping.py
@@ -1,11 +1,74 @@
 import pandas as pd
 import numpy as np
 from time import sleep
-from Functions import HTMLTableParser as hp
+# from Functions import HTMLTableParser
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from bs4 import BeautifulSoup
 import re
+
+
+# Defining the class and two functions that will allow us scraping the data and arranging it in a dataframe.
+# Credit for these function to Scott Rome. They can be found here:
+# https://srome.github.io/Parsing-HTML-Tables-in-Python-with-BeautifulSoup-and-pandas/
+class HTMLTableParser:
+
+    def parse_html(self):
+        sourcecode = driver.page_source
+        return BeautifulSoup(sourcecode, 'lxml')
+
+    def arrange_html(self, table):
+        n_columns = 0
+        n_rows = 0
+        column_names = []
+
+        # Find number of rows and columns
+        # we also find the column titles if we can
+        for row in table.find_all('tr'):
+
+            # Determine the number of rows in the table
+            td_tags = row.find_all('td')
+            if len(td_tags) > 0:
+                n_rows += 1
+                if n_columns == 0:
+                    # Set the number of columns for our table
+                    n_columns = len(td_tags)
+
+            # Handle column names if we find them
+            th_tags = row.find_all('th')
+            if len(th_tags) > 0 and len(column_names) == 0:
+                for th in th_tags:
+                    column_names.append(th.get_text())
+
+        # Safeguard on Column Titles
+        if len(column_names) > 0 and len(column_names) != n_columns:
+            raise Exception("Column titles do not match the number of columns")
+
+        columns = column_names if len(column_names) > 0 else range(0, n_columns)
+        df = pd.DataFrame(columns=columns,
+                          index=range(0, n_rows))
+        row_marker = 0
+        for row in table.find_all('tr'):
+            column_marker = 0
+            columns = row.find_all('td')
+            for column in columns:
+                df.iat[row_marker, column_marker] = column.get_text()
+                column_marker += 1
+            if len(columns) > 0:
+                row_marker += 1
+
+        # Convert to float if possible
+        for col in df:
+            try:
+                df[col] = df[col].astype(float)
+            except ValueError:
+                pass
+
+        return df
+
+
+hp = HTMLTableParser()
 
 # First, we set the season, current gameweek and previous gameweek variables
 season = '21'
--- xG Scatterplots.R
+++ xG Scatterplots.R
@@ -13,18 +13,35 @@ ggplot(data = sgw_data, aes(x = Player_xG, y = Goals.scored, color = Cost)) +
   scale_color_gradient(low = 'orange', high = 'red') +
   stat_poly_eq(formula = y ~ x, parse = T)
 
-# Cumulaive - xG vs. Goals scored
+# Cumulative - xG vs. Goals scored
 ggplot(data = cum_data, aes(x = Player_xG, y = Goals.scored)) +
   geom_text(data = cum_data, aes(label = Player, size = Goals.scored)) + 
   scale_size(range = c(1,4)) + 
   geom_smooth(method = 'lm') +
-  stat_poly_eq(formula = y ~ x, parse = T) + 
-  theme(legend.position = 'none')
+  stat_poly_eq(formula = y ~ x, parse = T, size = 5) + 
+  theme(legend.position = 'none') +
+  scale_x_continuous(name = 'xG', limits = c(0,12.5), breaks = seq(0, 13, 2)) + 
+  scale_y_continuous(name = 'Goals Scored', limits = c(0,13), breaks = seq(0, 13, 2))
 
-# Cumulaive - xA vs. Assists
+# Export plot
+ggsave(filename = 'xG_Goals.png',
+       path = './Visualizations',
+       width = 8,
+       height = 4.46666)
+
+# Cumulative - xA vs. Assists
 ggplot(data = cum_data, aes(x = Player_xA, y = Assists)) +
   geom_text(data = cum_data, aes(label = Player, size = Assists)) + 
   scale_size(range = c(1,4)) + 
   geom_smooth(method = 'lm') +
-  stat_poly_eq(formula = y ~ x, parse = T) + 
-  theme(legend.position = 'none')
+  stat_poly_eq(formula = y ~ x, parse = T, size = 5) + 
+  theme(legend.position = 'none') +
+  scale_x_continuous(name = 'xA', limits = c(0,8), breaks = seq(0, 8, 2)) + 
+  scale_y_continuous(name = 'Assists', limits = c(0,11), breaks = seq(0, 11, 2))
+
+# Export plot
+ggsave(filename = 'xA_Assists.png',
+       path = './Visualizations',
+       width = 8,
+       height = 4.46666)
+
