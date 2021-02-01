c6bbb9a82b22f27b911e9a4ef914515692cfe0fd
nirgodin <70273595+nirgodin@users.noreply.github.com>
2021-02-01 19:21:24 +0200

    GW 21 Scraping

--- Code/Merging.py
+++ Code/Merging.py
@@ -4,8 +4,8 @@ import re
 
 # First, we set the season, current gameweek and previous gameweek variables
 season = '21'
-previous_GW = '19'
-current_GW = '20'
+previous_GW = '20'
+current_GW = '21'
 
 # Import the previous and this week cumulative dataframes
 cum_prev_df = pd.read_csv(r'Data\FPL\FPL_S' + season + '_GW1_' + previous_GW + '.csv')
--- Code/Scraping.py
+++ Code/Scraping.py
@@ -10,7 +10,7 @@ hp = HTMLTableParser()
 
 # First, we set the season, current gameweek and previous gameweek variables
 season = '21'
-current_GW = '20'
+current_GW = '21'
 
 # Setting driver
 driver = webdriver.Chrome(r'C:\Users\nirgo\PycharmProjects\Fantasy\Browsers\chromedriver.exe')
@@ -69,21 +69,21 @@ for elem in scraping_lst:
     clk = 'menu.select_by_visible_text' + '(' + '"' + elem + '"' + ')'
     exec(clk)
     page = 0
-    # while page <= 19:
-    #     raw = hp.parse_html(driver=driver)
-    #     temp = hp.arrange_html(raw)
-    #     category_df.append(temp)
-    #     next_page.click()
-    #     page += 1
+    while page <= 21:
+        raw = hp.parse_html(driver=driver)
+        temp = hp.arrange_html(raw)
+        category_df.append(temp)
+        next_page.click()
+        page += 1
 
-    while (True):
-        try:
-            raw = hp.parse_html(driver=driver)
-            temp = hp.arrange_html(raw)
-            category_df.append(temp)
-            next_page.click()
-        except ElementClickInterceptedException as error:
-            break
+    # while (True):
+    #     try:
+    #         raw = hp.parse_html(driver=driver)
+    #         temp = hp.arrange_html(raw)
+    #         category_df.append(temp)
+    #         next_page.click()
+    #     except ElementClickInterceptedException as error:
+    #         break
     final = pd.concat(category_df)
     final.rename(columns={'**': elem}, inplace=True)
     df_lst.append(final)
