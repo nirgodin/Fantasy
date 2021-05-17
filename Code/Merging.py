25e60f3379fdd28b7215b32e6b668ae34513843e
nirgodin <70273595+nirgodin@users.noreply.github.com>
2021-05-17 12:18:09 +0300

    Scraping GW 31-36

--- Code/Merging.py
+++ Code/Merging.py
@@ -4,8 +4,8 @@ import re
 
 # First, we set the season, current gameweek and previous gameweek variables
 season = '21'
-previous_GW = '29'
-current_GW = '30'
+previous_GW = '35'
+current_GW = '36'
 
 # Import the previous and this week cumulative dataframes
 cum_prev_df = pd.read_csv(r'Data\FPL\FPL_S' + season + '_GW1_' + previous_GW + '.csv')
