af46763a408586717363613e40e057135f082b7c
nirgodin <70273595+nirgodin@users.noreply.github.com>
2021-07-31 19:16:25 +0300

    select menu button function

--- Code/crawler/webcontroller/web_controller.py
+++ Code/crawler/webcontroller/web_controller.py
@@ -1,10 +1,19 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.select import Select
+
+
 class WebController:
 
-    def __init__(self):
-        pass
+    def __init__(self, chromedriver: WebDriver):
+        self._driver = chromedriver
 
     @staticmethod
-    def _click_dropdown_menu(menu_button, category_name: str) -> None:
-        menu_button.select_by_visible_text(category_name)
+    def _click_dropdown_menu(select_menu_button: Select, category_name: str) -> None:
+        select_menu_button.select_by_visible_text(category_name)
 
         return None
+
+    def _get_select_menu_button(self, select_menu_xpath: str):
+        select_menu_button = self._driver.find_element_by_xpath(select_menu_xpath)
+
+        return select_menu_button
