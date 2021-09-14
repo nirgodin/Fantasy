a9572818a648daf5dacaa33751471a72a75cacda
nirgodin <70273595+nirgodin@users.noreply.github.com>
2021-09-14 23:07:44 +0300

    GW 4 scraping

--- Code/Scraping.py
+++ Code/Scraping.py
@@ -10,7 +10,7 @@ hp = HTMLTableParser()
 
 # First, we set the season, current gameweek and previous gameweek variables
 season = '22'
-current_GW = '2'
+current_GW = '4'
 
 # Setting driver
 driver = webdriver.Chrome(r'Browsers\chromedriver.exe')
